559b58cca913cbdf793362a0ce508d571cb3570a
Dustin Spicuzza <dustin@virtualroadside.com>
2009-01-16 03:45:23 +0000

    Fixed logical errors, will test on saturday
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@32 eb211378-2a44-443b-ae4a-d45323d45073

--- trunk/PIDCompassDrive/MyRobot.cpp
+++ trunk/PIDCompassDrive/MyRobot.cpp
@@ -1,5 +1,6 @@
 #include "WPILib.h"
-
+#include <cmath>
+#include "math_definitions.h"
 /**
  * This is a demo program showing the use of the RobotBase class.
  * The SimpleRobot class is the base of a robot application that will automatically call your
@@ -11,15 +12,18 @@ class RobotDemo : public SimpleRobot
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
 	
+	Gyro gyro;
 	
-	PIDCompassDrive pidCompassDrive;
+	// moving right is positive
 
 public:
 	RobotDemo(void):
 		myRobot(1, 2),	// these must be initialized in the same order
-		stick(1)		// as they are declared above.
+		stick(1),		// as they are declared above.
+		gyro(1)
 	{
 		GetWatchdog().SetExpiration(100);
+		gyro.SetSensitivity(0.007);
 	}
 
 	/**
@@ -27,10 +31,10 @@ public:
 	 */
 	void Autonomous(void)
 	{
-		GetWatchdog().SetEnabled(false);
-		myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
+		//GetWatchdog().SetEnabled(false);
+		//myRobot.Drive(0.5, 0.0); 	// drive forwards half speed
+		//Wait(2.0); 				//    for 2 seconds
+		//myRobot.Drive(0.0, 0.0); 	// stop robot
 	}
 
 	/**
@@ -38,16 +42,48 @@ public:
 	 */
 	void OperatorControl(void)
 	{
+		printf("Entered OperatorControl\n");
+	
+		float time = GetTime();
+		
 		GetWatchdog().SetEnabled(true);
 		while (IsOperatorControl())
 		{
 			GetWatchdog().Feed();
 			
-			// compass drive II
-			float speed = 
+			// compass drive II (one stick)
+			float x = stick.GetX();
+			float y = stick.GetY() * -1;
 			
+			// convert rectangular coordinates to polar
+			float speed = hypot(x, y) * M_SQRT1_2;
+			float desired_heading = (atan2( y , x ) * (180 / M_PI)) + 90;
+			if (desired_heading < 0) desired_heading = 360 - desired_heading;
 			
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+			// get the robot heading (0 to 360 degrees)
+			float heading = fmod(gyro.GetAngle(), 360.0);
+			if (heading < 0) heading = 360 - heading;
+			
+			// convert the difference to a value between 1 and -1
+			float rate_of_turn = (desired_heading - heading) * (1/360);
+			
+			// allow some absolute error
+			if (fabs(heading - desired_heading) < 2.0)
+				rate_of_turn = 0;
+			
+			// don't bother with this until you verify the params
+			//myRobot.Drive(speed, rate_of_turn);
+			
+			// debug information
+			if (GetTime() - time > 0.25)
+			{
+				printf("X: %.3f, Y: %.3f S: %.3f DH: %.3f, H: %.3f rt: %.3f\r", 
+						x, y
+						speed, desired_heading,
+						heading,
+						rate_of_turn);
+				time = GetTime();
+			}
 		}
 	}
 };

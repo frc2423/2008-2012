6c46c13c197356a6d64f9c2c1d23371638610648
Dustin Spicuzza <dustin@virtualroadside.com>
2009-01-31 08:38:56 +0000

    Commit PositionTest via Subclipse
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@78 eb211378-2a44-443b-ae4a-d45323d45073

---
+++ PositionTest/KwarqsDevices.h
@@ -0,0 +1,33 @@
+/**
+	\file 		KwarqsDevices.h
+	\author 	Dustin Spicuzza: last changed by $Author: dspicuzz $
+	\date 		Last changed on $Date: 2009-01-25 03:23:53 -0500 (Sun, 25 Jan 2009) $
+	\version 	$Rev: 46 $
+
+	This file contains initialization definitions that define channel number
+	or other needed data for motors and sensors. The idea is to keep all of
+	the configuration data in a central place so that its easier to modify
+	later on if we need to move things around. 
+*/
+
+#ifndef FRAMEWORK_KWARQS_DEVICES_H
+#define FRAMEWORK_KWARQS_DEVICES_H
+
+// analog sidecar
+#define GYRO_CHANNEL				1
+
+#define ACCELEROMETER_X_CHANNEL		3
+#define ACCELEROMETER_Y_CHANNEL		4	
+
+// digital sidecar 
+
+// pwm channels
+#define LEFT_MOTOR_PWM				1
+#define RIGHT_MOTOR_PWM				2
+
+
+// operator station
+#define FIRST_JOYSTICK_PORT			1
+#define SECOND_JOYSTICK_PORT		2
+
+#endif

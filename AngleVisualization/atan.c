7b08f0eb1882ee56747174354c8ee0d36fcb70b5
Dustin Spicuzza <dustin@virtualroadside.com>
2009-02-10 05:31:08 +0000

    Added modified angle visualization program
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@119 eb211378-2a44-443b-ae4a-d45323d45073

--- AngleVisualization/atan.c
+++ AngleVisualization/atan.c
@@ -1,23 +1,48 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+
+int main(int argc, char ** argv)
 {
-	for (double y = 1.0; y > -1.1; y -= 0.25)
-	{
-		for (double x = -1.0; x < 1.1; x += 0.25)
-		{
+	double current_angle = argc < 2 ? 0 : strtol(argv[1], NULL, 10);
 
-			double angle = (atan2(y, x) * (180/M_PI) - 90.0 ) * -1;
+	for (current_angle = 0; current_angle < 361.0; current_angle += 15.0)
+	{
+		printf("\nAngle: %f\n", current_angle);
+	
+		for (double y = 1.0; y > -1.1; y -= 0.25)
+		{
+			for (double x = -1.0; x < 1.1; x += 0.25)
+			{
+
+				double desired_angle = (atan2(y, x) * (180/M_PI) - 90.0 ) * -1;			
+				if (desired_angle < 0) desired_angle += 360;
+
+				
+
+				double rotateValue = (desired_angle - current_angle );
+					
+				if (rotateValue > 180)
+					rotateValue = rotateValue - 360.0;
+				else if (rotateValue < -180)
+					rotateValue = rotateValue + 360.0;
+					
+				if (fabs(rotateValue) < 15.0 )
+					rotateValue = 0;
+				
+				rotateValue = rotateValue / 90.0;
+				if (rotateValue > 1.0)
+					rotateValue = 1.0;
+				else if (rotateValue < -1.0)
+					rotateValue = -1.0;
+				
+				printf("%4.4f ", rotateValue);
+			}
 			
-			if (angle < 0) angle += 360;
-			//angle -= 90.0;
-			
-			printf("%4.0f ", angle);
+			printf("\n");
 		}
-		
-		printf("\n");
 	}
 }
 

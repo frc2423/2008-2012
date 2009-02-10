c198015129ac01283eceb464cec78309de4542b3
Dustin Spicuzza <dustin@virtualroadside.com>
2009-02-10 07:17:19 +0000

    Uploaded KwarqsDevices definitions
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@120 eb211378-2a44-443b-ae4a-d45323d45073

--- ProductionCode/trunk/Framework/KwarqsDevices.h
+++ ProductionCode/trunk/Framework/KwarqsDevices.h
@@ -13,21 +13,80 @@
 #ifndef FRAMEWORK_KWARQS_DEVICES_H
 #define FRAMEWORK_KWARQS_DEVICES_H
 
+// operator station
+#define FIRST_JOYSTICK_PORT			1
+#define SECOND_JOYSTICK_PORT		2
+
 // analog sidecar
 #define GYRO_CHANNEL				1
 
 #define ACCELEROMETER_X_CHANNEL		3
 #define ACCELEROMETER_Y_CHANNEL		4	
 
-// digital sidecar 
+// digital sidecar
+//
+//	LF: left-front
+//	LR: left-rear
+//	RF: right-front
+//	RR: right-rear
+//
 
-// pwm channels
-#define LEFT_MOTOR_PWM				1
-#define RIGHT_MOTOR_PWM				2
+// drive motors
+
+#define MOTOR_LF_SLOT				4
+#define MOTOR_LR_SLOT				4
+#define MOTOR_RF_SLOT				5
+#define MOTOR_RR_SLOT				5
+
+#define PWM_LF_JAGUAR				1
+#define PWM_LR_JAGUAR				2
+#define PWM_RF_JAGUAR				1
+#define PWM_RR_JAGUAR				2
+
+// encoder channels: motor
+#define ENCODER_MOTOR_LF_1			1
+#define ENCODER_MOTOR_LF_2			1
+#define ENCODER_MOTOR_LR_1			1
+#define ENCODER_MOTOR_LR_2			1
+#define ENCODER_MOTOR_RF_1			1
+#define ENCODER_MOTOR_RF_2			1
+#define ENCODER_MOTOR_RR_1			1
+#define ENCODER_MOTOR_RR_2			1
 
 
-// operator station
-#define FIRST_JOYSTICK_PORT			1
-#define SECOND_JOYSTICK_PORT		2
+// servos
+#define SERVO_LF_SLOT				4
+#define SERVO_LR_SLOT				4
+#define SERVO_RF_SLOT				5
+#define SERVO_RR_SLOT				5
+
+#define PWM_LF_VICTOR				3
+#define PWM_LR_VICTOR				4
+#define PWM_RF_VICTOR				3
+#define PWM_RR_VICTOR				4
+
+// encoder channels: servo
+#define ENCODER_SERVO_LF_1			1
+#define ENCODER_SERVO_LF_2			1
+#define ENCODER_SERVO_LR_1			1
+#define ENCODER_SERVO_LR_2			1
+#define ENCODER_SERVO_RF_1			1
+#define ENCODER_SERVO_RF_2			1
+#define ENCODER_SERVO_RR_1			1
+#define ENCODER_SERVO_RR_2			1
+
+// servo calibration signals
+#define SERVO_CAL_LF				1
+#define SERVO_CAL_LR				1
+#define SERVO_CAL_RF				1
+#define SERVO_CAL_RR				1
+
+// servo constants
+#define SERVO_F_SCALE				45
+#define SERVO_F_TICKS				1000
+
+#define SERVO_R_SCALE				45
+#define SERVO_R_TICKS				1000
+
 
 #endif

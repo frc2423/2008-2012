87f45871e86473d77ad628968ebc42adf740ec5d
Dustin Spicuzza <dustin@virtualroadside.com>
2009-02-15 08:47:00 +0000

    Added status to ServoTest
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@151 eb211378-2a44-443b-ae4a-d45323d45073

--- ServoTest/MyRobot.cpp
+++ ServoTest/MyRobot.cpp
@@ -1,7 +1,9 @@
 #include "WPILib.h"
 
 #include <math.h>
+#include "Framework/KwarqsDSLCDStatus.h"
 #include "Framework/KwarqsWheelServo.h"
+#include "Framework/KwarqsConstants.h"
 #include "Framework/math.h"
 
 /**
@@ -12,26 +14,22 @@
  */ 
 class RobotDemo : public SimpleRobot
 {
-	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+	KwarqsDSLCDStatus 	status;		// status engine
+	Joystick 			stick; 		// only joystick
 	
 	KwarqsWheelServo servo;
 	
 public:
 	// slot, pwm, e1, e2, cal
 	RobotDemo(void):
-		myRobot(1, 2),
 		stick(1),
-		servo(6, 4, 3, 4, 10, 25.0, 3272, 0, true)
+		
+		servo(SERVO_LF_PARAMETERS)
+		//servo(SERVO_LR_PARAMETERS)
+		//servo(SERVO_RF_PARAMETERS)
+		//servo(SERVO_RR_PARAMETERS)
 	{
-		// LF: 4, 2, 1, 2, 9, 25.0, 3272, 0
-		// LR: 4, 4, 3, 4, 10, 25.0, 3272, 0, true
-		// 
-		// RF: 6, 2, 1, 2, 11, 25.0, 3272, 0
-		// RR: 6, 4, 3, 4, 10, 25.0, 3272, 0, true
-		
-		GetWatchdog().SetExpiration(100);	
-		
+		GetWatchdog().SetExpiration(100);		
 	}
 
 	/**
@@ -45,7 +43,7 @@ public:
 	{
 		double y = stick.GetY() * -1, x = stick.GetX();
 		
-		double speed = sqrt(x*x + y*y);
+		double speed = __hypot(x, y);
 		
 		double desired_angle = (atan2(y, x) * (180/M_PI) - 90.0 );			
 		if (desired_angle < 0) desired_angle += 360;
@@ -64,10 +62,7 @@ public:
 	{
 		double time = GetTime();
 		
-		double lastP = 0, lastI = 0;
-		
-		bool disabled = false;
-		
+		double lastP = 0, lastI = 0, p = 0, i = 0;
 		double setPoint = 0;
 		
 		GetWatchdog().SetEnabled(true);
@@ -81,53 +76,39 @@ public:
 			
 			//double setPoint = DriverStation::GetInstance()->GetAnalogIn(1);
 			
-			double p = DriverStation::GetInstance()->GetAnalogIn(2);
-			double i = DriverStation::GetInstance()->GetAnalogIn(3);
+			//double p = DriverStation::GetInstance()->GetAnalogIn(2);
+			//double i = DriverStation::GetInstance()->GetAnalogIn(3);
 			
+			// do calibration
 			if (stick.GetTrigger())
-			{
-				// control motor manually
-				if (!disabled)
-					servo.Disable();
-					
-				servo.m_motor.Set(stick.GetY()*-1);
-				disabled = true;
-			}
-			else
-			{	
-				if (disabled)
-				{
-					servo.Enable();
-					disabled = true;
-				}
+				servo.Calibrate();
 			
-				/*
-				if (p > 1000.0)
-					p = 1000.0;
-				p = p / 1000.0;
-				
-				if (i > 1000.0)
-					i = 1000.0;
-				i = i / 1000.0;
-				
-				// only change if there is a significant difference
-				if (fabs(p - lastP) > 0.05 || fabs(i - lastI) > 0.05)
-				{
-					servo.TuneParameters(p, i, 0);
-					lastP = p;
-					lastI = i;
-				}
-				 */
-				
-				// only change angle if desired
-				if (ConvertStickToAngle(setPoint))
-					servo.SetAngle(setPoint);
-				
-				//if (setPoint > 1000.0)
-				//	setPoint = 1000.0;
-				
-				//setPoint = ceil(setPoint /(1000.0/ 360.0));
+			/*
+			if (p > 1000.0)
+				p = 1000.0;
+			p = p / 1000.0;
+			
+			if (i > 1000.0)
+				i = 1000.0;
+			i = i / 1000.0;
+			
+			// only change if there is a significant difference
+			if (fabs(p - lastP) > 0.05 || fabs(i - lastI) > 0.05)
+			{
+				servo.TuneParameters(p, i, 0);
+				lastP = p;
+				lastI = i;
 			}
+			 */
+			
+			// only change angle if desired
+			if (ConvertStickToAngle(setPoint))
+				servo.SetAngle(setPoint);
+			
+			//if (setPoint > 1000.0)
+			//	setPoint = 1000.0;
+			
+			//setPoint = ceil(setPoint /(1000.0/ 360.0));
 			
 			if (GetTime() - time > 0.25)
 			{

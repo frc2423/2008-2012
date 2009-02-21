ff7e8bda39f3045fa6c08dbe261d8239abbdbf0c
Dustin Spicuzza <dustin@virtualroadside.com>
2009-02-21 18:10:07 +0000

    Added different counter mechanism for measuring low period
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@213 eb211378-2a44-443b-ae4a-d45323d45073

--- PWMAccelerometerTest/MyRobot.cpp
+++ PWMAccelerometerTest/MyRobot.cpp
@@ -14,22 +14,37 @@ class RobotDemo : public SimpleRobot
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
 
-	Counter countX, countY;
+	Counter countXhi, countXlow, countYhi, countYlow;
 	
 public:
 	RobotDemo(void):
 		myRobot(1, 2),	// these must be initialized in the same order
 		stick(1),
-		countX(3), countY(4)
 	{
 		GetWatchdog().SetExpiration(0.1);
 		
-		// setup the counter
-		countX.SetPulseLengthMode(1.0);
-		countY.SetPulseLengthMode(1.0);
+		// specify the source
+		countXhi.SetUpSource(3);
+		countXlow.SetUpSource(3);
+		countYhi.SetUpSource(4);
+		countYlow.SetUpSource(4);
 		
-		countX.Start();
-		countY.Start();
+		// specify the edge
+		countXhi.SetUpSourceEdge(true, false);
+		countXlow.SetUpSourceEdge(false, true);
+		countXhi.SetUpSourceEdge(true, false);
+		countYlow.SetUpSourceEdge(false, true);
+		
+		// setup the counters
+		countXhi.SetPulseLengthMode(1.0);
+		countXlow.SetPulseLengthMode(1.0);
+		countYhi.SetPulseLengthMode(1.0);
+		countYlow.SetPulseLengthMode(1.0);
+		
+		countXhi.Start();
+		countXlow.Start();
+		countYhi.Start();
+		countYlow.Start();
 	}
 
 	/**
@@ -38,6 +53,7 @@ public:
 	void OperatorControl(void)
 	{
 		double tm = GetTime();
+		double vx = 0.0, vy = 0.0;
 		
 		GetWatchdog().SetEnabled(true);
 		while (IsOperatorControl())
@@ -45,26 +61,37 @@ public:
 			GetWatchdog().Feed();
 			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
 			
-			if (GetTime() - tm > 0.25)
+			if (GetTime() - tm > 0.1)
 			{
-				double ax = countX.GetPeriod();
-				double ay = countY.GetPeriod();
+				double axH = countXhi.GetPeriod();
+				double axL = countXlow.GetPeriod();
+				double ayH = countYhi.GetPeriod();
+				double ayL = countYlow.GetPeriod();
 				
 				// convert to m/s^2
-				ax = (((ax / 0.01) - .5) * 8.0) * 9.81;
-				ay = (((ay / 0.01) - .5) * 8.0) * 9.81;
+				double ax = (((axH / (axH + axL)) - .5) * 8.0) * 9.81;
+				double ay = (((ayH / (ayH + ayL)) - .5) * 8.0) * 9.81;
+								
+				// or
+				// ax = (((axH / 0.01) - .5) * 8.0) * 9.81;
+				// ay = (((ayH / 0.01) - .5) * 8.0) * 9.81;
 								
 				DriverStationLCD * lcd = DriverStationLCD::GetInstance();
 				
 				lcd->PrintfLine(DriverStationLCD::kUser_Line3, "ax: %f m/s^2", ax);
-				lcd->PrintfLine(DriverStationLCD::kUser_Line4, "ay: %f m/s^2", ay);
+				lcd->PrintfLine(DriverStationLCD::kUser_Line4, "%.6f %.6f", axH, axL);
+				lcd->PrintfLine(DriverStationLCD::kUser_Line5, "ay: %f m/s^2", ay);
+				lcd->PrintfLine(DriverStationLCD::kUser_Line6, "%.6f %.6f", ayH, ayL);
+				
+				// euler' integration method.. bad bad bad
+				vx += ax / (axH + axL);
+				vy += ay / (ayH + ayL);
 				//lcd->PrintfLine(DriverStationLCD::kUser_Line5, "vx: %.1f", vx);
 				//lcd->PrintfLine(DriverStationLCD::kUser_Line6, "vy: %.1f", vy);
 				
+				lcd->UpdateLCD();
 				tm = GetTime();
 			}
-			
-			Wait(0.005);				// wait for a motor update time
 		}
 	}
 };

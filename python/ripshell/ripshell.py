a365076269d0dfb18aeda326559e51ffd3fc495a
Dustin Spicuzza <dustin@virtualroadside.com>
2011-01-13 06:36:50 +0000

    Improvements to RIPShell to make it a bit easier to use and less annoying
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@469 eb211378-2a44-443b-ae4a-d45323d45073

--- python/ripshell/ripshell.py
+++ python/ripshell/ripshell.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 # Copyright (c) 2006 Guido Wesdorp. All rights reserved.
-#  --> ported to Python3, Dustin Spicuzza (10/28/2010)
+#  --> ported to Python3 for FRC, Dustin Spicuzza (10/28/2010)
+#  --> Other fixes, Jan/2011
 
 # This software is distributed under the terms of the RIPShell
 # License. See LICENSE.txt for license text.
@@ -19,11 +20,14 @@ import sys
 import _thread
 import traceback
 
+
 class config:
     server_ip = '0.0.0.0'
-    port = 8002
+    port = 10101
 
-__version__ = '0.2 SVN'
+__version__ = '0.3 FRC'
+
+
 
 class STDFilePointers:
     """proxy for file pointers
@@ -60,10 +64,12 @@ class RIPServer:
         from and to the client, or runs an interactive interpreter loop
     """
 
-    banner = ('Python %s\n'
+    banner = (  'Python %s\n'
                 'Remote Interactive Python Shell v%s\n'
-                'Type "help", "copyright", "credits" or "license" '
-                    'for more information.' % (sys.version, __version__))
+                'Type "help", "copyright", "credits" or "license" for more information.\n\n' 
+                '**To stop the server: raise SystemExit("quit")\n' 
+                % (sys.version, __version__)
+            )
 
     def __init__(self, config):
         """open a socket and start waiting for connections"""
@@ -152,8 +158,12 @@ class RIPClient:
             which will be executed on the server
         """
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
         try:
+            s.settimeout(10.0) # 10 second timeout for connect
             s.connect((server_ip, config.port))
+            s.settimeout(None)
+            
             if filedata:
                 self.execfile(s, filedata)
             else:
@@ -191,26 +201,46 @@ class RIPClient:
             sys.stdout.flush()
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('Starting server...')
-        RIPServer(config) # blocks until done
-        sys.exit(0)
-    elif len(sys.argv) == 2 and sys.argv[1] not in ['-h', '?', '--help']:
-        filedata = None
-        #if sys.argv[1] != '-':
-        #    fp = open(sys.argv[1])
-        #    try:
-        #        filedata = fp.read()
-        #    finally:
-        #        fp.close()
-        RIPClient(sys.argv[1], config, filedata) # blocks until done
-        sys.exit(0)
-    print('Usage: %s [-|<filename>]' % (sys.argv[0],))
-    print()
-    print ('Without any command line arguments, the server is started, which\n'
-            'allows executing Python code from connected clients. When the\n'
-            'first argument is a dash (-), the client connects to the server\n'
-            'and requests it to start an interactive loop. Any other\n'
-            'argument will be considered a path to a Python script, which\n'
-            'will be executed on the server.\n')
+
+    from optparse import OptionParser
+
+    usage = ('usage: %prog [-p] -l | remote_server\n'
+            '\n'
+            'RIPShell allows you to access a python interpreter running on a remote\n'
+            'machine over an unencrypted socket. Use ripshell in listening mode\n'
+            '(use -l) or to connect to a ripshell server')
+    
+    parser = OptionParser(usage)
+
+    parser.add_option('-l', '--listen', action='store_true', dest='listen', default=False, 
+                            help='Listen for a remote connection' )
+                            
+    parser.add_option('-p', '--port', action='store', type='int', dest='port', default=config.port,
+                            help='port for ripshell to use')
+    
+    (options, args) = parser.parse_args()
+    
+    if options.port <= 0 or options.port >= 65535:
+        parser.error('Invalid port specified')
+    
+    if options.listen:
+        if len(args) > 0:
+            parser.error("-l cannot use be specified with a hostname")
+            
+        print('RIPShell server listening on port %d' % options.port)
+        config.port = options.port
+        
+        # blocks until done
+        RIPServer(config) 
+            
+    elif len(args) == 1:
+    
+        config.server_ip = args[0]
+        config.port = options.port
+        
+        # blocks until done
+        RIPClient( config.server_ip, config, None )
+        
+    else:
+        parser.error( 'Invalid arguments specified.' )
 

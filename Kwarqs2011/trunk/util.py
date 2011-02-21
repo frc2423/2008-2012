6fc5a310fe0f7bda420d78af8a54a8e6520325da
Dustin Spicuzza <dustin@virtualroadside.com>
2011-02-21 05:28:45 +0000

    Fixed PrintTimer to not print out a million things when the robot first starts...
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@488 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2011/trunk/util.py
+++ Kwarqs2011/trunk/util.py
@@ -5,9 +5,12 @@ class PrintTimer(object):
 
     def __init__(self, timeout=1.0):
         self.timer = wpilib.Timer()
-        self.timer.Start()
         self.timeout = timeout
+        self.started = False
         
     def should_print(self):
+        if not self.started:
+            self.timer.Start()
+            self.started = True
         return self.timer.HasPeriodPassed(self.timeout)
 

9b5b175bf439025ebb95bfabadfa64efa6721935
Dustin Spicuzza <dustin@virtualroadside.com>
2011-04-06 01:37:40 +0000

    Arm adjustment program adjusted to use potentiometer mode
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@494 eb211378-2a44-443b-ae4a-d45323d45073

--- python/arm_adjustment/robot.py
+++ python/arm_adjustment/robot.py
@@ -6,8 +6,13 @@ import wpilib
 stick1 = wpilib.Joystick(1)
 stick2 = wpilib.Joystick(2)
 
-THUMP_MAX_POSITION = 1
-THUMP_MIN_POSITION = 0
+THUMP_MAX_POSITION = .82
+THUMP_MIN_POSITION = .50
+
+# PID constants
+ARM_P = 1000.0
+ARM_I = 0.2
+ARM_D = 0.0
 
 
 class MyRobot(wpilib.SimpleRobot):
@@ -17,13 +22,12 @@ class MyRobot(wpilib.SimpleRobot):
         self.vertical_motor = wpilib.CANJaguar(10)
         self.thump_motor = wpilib.CANJaguar(11)
         
-        #self.thump_motor = wpilib.CANJaguar(11, wpilib.CANJaguar.kPosition )
+        self.position = False
+        
+        # self.thump_motor = wpilib.CANJaguar(11, wpilib.CANJaguar.kPosition )
         # self.thump_motor.SetPID( .95, 0.0, 0.0 )
         
-        self.thump_motor.SetPositionReference( wpilib.CANJaguar.kPosRef_Potentiometer )
-        self.thump_motor.ConfigPotentiometerTurns( 1 )
         
-        # self.thump_motor.ConfigSoftPositionLimits( THUMP_MIN_POSITION, THUMP_MAX_POSITION )
         # self.thump_motor.EnableControl()
 
         
@@ -41,6 +45,40 @@ class MyRobot(wpilib.SimpleRobot):
         while self.IsAutonomous() and self.IsEnabled():
             wpilib.Wait(0.01)
             
+            
+    def toggle_mode(self):
+    
+        if self.thump_motor.GetControlMode() == wpilib.CANJaguar.kPosition:
+            self.set_jaguar_control_mode( wpilib.CANJaguar.kPercentVbus )
+        else:
+            self.set_jaguar_control_mode( wpilib.CANJaguar.kPosition )
+        
+    def set_jaguar_control_mode(self, mode):
+        '''Sets the control mode for the Jaguar'''
+    
+        if self.thump_motor.GetControlMode() != mode:
+        
+            print( "Trying to change the mode to %d" % int(mode) )
+        
+            self.thump_motor.ChangeControlMode(mode)
+        
+            if mode == wpilib.CANJaguar.kPosition:
+                
+                self.position = True
+                
+                self.thump_motor.SetPositionReference( wpilib.CANJaguar.kPosRef_Potentiometer )
+                self.thump_motor.ConfigPotentiometerTurns( 1 )
+                
+                #self.thump_motor.ConfigSoftPositionLimits( THUMP_MIN_POSITION, THUMP_MAX_POSITION )
+                
+                #self.thump_motor.ConfigEncoderCodesPerRev( ENCODER_TURNS_PER_REVOLUTION )
+                #self.thump_motor.SetPositionReference( wpilib.CANJaguar.kPosRef_QuadEncoder )
+                self.thump_motor.SetPID( ARM_P, ARM_I, ARM_D )
+        
+                self.thump_motor.EnableControl()
+            else:
+                self.position = False
+                self.thump_motor.DisableControl()
             
     def _translate_z(self, z):
     
@@ -66,10 +104,21 @@ class MyRobot(wpilib.SimpleRobot):
             dog.Feed()
             
             # vertical arm code
-            if stick2.GetTrigger():
-                self.vertical_motor.Set( stick2.GetY() )
+            if self.position:
+                self.thump_motor.Set( self._translate_z( stick2.GetZ() ) )
             else:
-                self.vertical_motor.Set( 0.0 )
+                if stick2.GetTrigger():
+                    self.thump_motor.Set( stick2.GetY() )
+                else:
+                    self.thump_motor.Set( 0.0 )
+            
+            #self.thump_motor.Set( stick2.GetZ() )
+                
+            if stick2.GetTop() and not on:
+                on = True
+                self.toggle_mode()
+            elif not stick2.GetTop():
+                on = False
                 
             if timer.HasPeriodPassed( 1.0 ):
                 print( "Thump position: %f; Current: %f; Voltage: %f; Set: %f" % ( \
@@ -78,10 +127,10 @@ class MyRobot(wpilib.SimpleRobot):
                     self.thump_motor.GetOutputVoltage(),
                     self.thump_motor.Get() ))
                 
-            if stick1.GetTrigger():
-                self.thump_motor.Set( stick1.GetY() )
-            else:
-                self.thump_motor.Set( 0.0 )
+            #if stick1.GetTrigger():
+            #    self.thump_motor.Set( stick1.GetY() )
+            #else:
+            #    self.thump_motor.Set( 0.0 )
                 
             # self.thump_motor.Set( self._translate_z( stick1.GetZ() ))
             

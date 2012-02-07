efc75def29ff7dd4cc9070d790d520555525ca39
Dustin Spicuzza <dustin@virtualroadside.com>
2012-02-07 18:34:02 +0000

    Updated the installer to work with the latest RobotPy installer
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@528 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/basket_tracker/install.py
+++ Kwarqs2012/basket_tracker/install.py
@@ -2,15 +2,37 @@
 import os
 import imp
 
-install = imp.load_source( 'install', 'C:\\WindRiver\\workspace\\robotpy\\utilities\\installer\\install.py' )
+try:
+    installer = imp.load_source( 'install', 'C:\\WindRiver\\workspace\\robotpy\\utilities\\installer\\install.py' )
+except IOError, e:
+    print( "Error loading the RobotPy installer library. (%s)\n\n" % e +
+           "-- Is RobotPy source installed at 'C:\\WindRiver\\workspace\\robotpy'?" )
+    exit(1)
+    
+robot_host = '10.24.23.2'
+    
+print( 'Connecting to robot host %s' % robot_host )
 
-installer = install.RobotCodeInstaller( '10.24.23.2' )
+try:
+    server = installer.RobotCodeInstaller( robot_host )
+except Exception as e:
+    print( "Error connecting to remote host: %s" % e )
+    exit(1)
 
 local_root = os.path.dirname(__file__)
 remote_root = '/lib/python3.2/site-packages'
 
 print( 'Beginning upload of basket tracker' )
 
-installer.upload_file( local_root, remote_root, '_basket_tracker.pyd' )
-installer.upload_file( local_root, remote_root, 'basket_tracker.py' )
-installer.close()
+# verify the directories actually exist
+server.create_remote_directory( remote_root )
+server.delete_remote( remote_root + '/__pycache__' )
+
+# upload the files
+server.upload_file( remote_root, local_root, '_basket_tracker.pyd' )
+server.upload_file( remote_root, local_root, 'basket_tracker.py' )
+
+print( 'Upload complete' )
+
+# close the installer
+server.close()

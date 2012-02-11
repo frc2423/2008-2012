995ffda4207072068e5bd9c02d4114ccc8d87235
Dylan Palmer <ditypa@gmail.com>
2012-02-11 22:22:48 +0000

    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@544 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/source/robot.py
+++ Kwarqs2012/source/robot.py
@@ -5,31 +5,33 @@ try:
     import wpilib
 except ImportError:
     import fake_wpilib as wpilib
+import robotmanager
 
+#Hardware Initilization Values
+rampArmMotorNum = 3
+
+robotManager = robotmanager( rampArmMotorNum)
 
 stick1 = wpilib.Joystick(1)
 stick2 = wpilib.Joystick(2)
 
+driveMotor1 = Jaguar(1)
+driveMotor2 = Jaguar(2)
+
 # this is the motor attached to the encoder
-motor = wpilib.Jaguar(3)
-encoder = wpilib.Encoder(10,11)
 
-motor1 = wpilib.Jaguar(1)
-motor2 = wpilib.CANJaguar(10)
 
-drive = wpilib.RobotDrive(motor1, motor2)
-drive.SetSafetyEnabled(False)
-drive.SetInvertedMotor( wpilib.RobotDrive.kRearLeftMotor, False )
-drive.SetInvertedMotor( wpilib.RobotDrive.kRearRightMotor, True )
-spike1 = wpilib.Relay(1) 
-spike2 = wpilib.Relay(3)
+drive = wpilib.RobotDrive( driveMotor1, driveMotor2)
+
+'''spike1 = wpilib.Relay(1) 
+spike2 = wpilib.Relay(3)'''
 
 class MyRobot(wpilib.SimpleRobot):
 
     def __init__(self):
         wpilib.SimpleRobot.__init__(self)
         encoder.Start()
-
+    
     def Disabled(self):
         
         print("MyRobot::Disabled()")
@@ -42,7 +44,8 @@ class MyRobot(wpilib.SimpleRobot):
         while self.IsAutonomous() and self.IsEnabled():
             wpilib.Wait(0.01)
             
-            
+    
+     
     def OperatorControl(self):
     
         print("MyRobot::OperatorControl()")
@@ -55,43 +58,32 @@ class MyRobot(wpilib.SimpleRobot):
         timer = wpilib.Timer()
         timer.Start()
         
-        on = False
-
         # main loop
         while self.IsOperatorControl() and self.IsEnabled():
-   
-            dog.Feed()
-            if stick1.GetTrigger():
-                motor.Set( 0 )
-                motor1.Set( stick1.GetZ() )
-                motor2.Set( stick1.GetZ() * -1.0 )
-            elif stick2.GetTop():
-                motor.Set( 0 )
-                drive.ArcadeDrive( stick2 )
-                
-            else:
-                motor.Set( 0 )
-                motor1.Set( 0 )
-                motor2.Set( 0 )
-               
-            if timer.HasPeriodPassed( 1.0 ):
-                print( "Motor: %f,%f,%f; Encoder: %d" % ( motor.Get(), motor1.Get(), motor2.Get(), encoder.Get() ) )
             
-            if stick1.GetRawButton(6): 
-                spike1.Set(wpilib.Relay.kForward)
-            elif stick1.GetRawButton(7):               
-               spike1.Set(wpilib.Relay.kReverse)
-            else:
-                spike1.Set(wpilib.Relay.kOff)
-                
-                
-                
-            if stick1.GetRawButton(10): 
-                spike2.Set(wpilib.Relay.kForward)
-            elif stick1.GetRawButton(11):               
-               spike2.Set(wpilib.Relay.kReverse)
-            else:
-                spike2.Set(wpilib.Relay.kOff)
+            dog.Feed()            
+            
+            drive.ArcadeDrive(stick1)
+            
+            if stick1.GetTop(): # makes the arm go down --- when button not pressed, the arm makes its ascent back up
+                RobotManager.LowerRamp()
+          
+            if stick1.GetTrigger(): #this is to fire the ball
+                RobotManager.ShootIfReady()
+            
+            ''' 
+            this is for the manuel override for the aiming, its sets a state when a button is pressed to initiate it.
+            
+            if stick2.GetRawButton(7) 
+                state = manual 
+               
+            else state = automatic
+            '''
+               
+               #this prints values so we know what's going on
+            if timer.HasPeriodPassed( 1.0 ):
+                print( "Motor: %f,%f" % ( driveMotor1.Get(), driveMotor2.Get()  ) )
+            
             
             wpilib.Wait(0.04)
                 

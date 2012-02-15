7c2d4f48323122ed54c8a665e5a538aa3a1a5b75
Ghi Coulter-de Wit <thegreatspiritofhoole@gmail.com>
2012-02-15 02:04:30 +0000

    I finished the ShooterWheel.py and am more then half way done on the ShooterAngle.py.
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@567 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/source/components/ShooterAngle.py
+++ Kwarqs2012/source/components/ShooterAngle.py
@@ -2,23 +2,22 @@ class VerticalAngle(object):
     
     
     
-    def __init__(self):
+    def __init__(self,degrees,mNumber):
         self.verticalGoalAngle = 0
         self.verticalCurrentAngle = 0 
+        self.motor = CANJaguar(mNumber)
     
     #set angle to angle given in goalAngle
-    def SetGoal(self, degrees)
+    def SetGoal(self,degrees)
         self.goalVeticalAngle = degrees
       
-
-    
     
     #check if GoalAngle = CurrentAngle
     def IsReady()
         if verticalGoalAngle == verticalCurrentAngle:
+            return True
             
-    
     
     #update current and goal angles
     def Update()
-        
+        self.motor.set
--- Kwarqs2012/source/components/ShooterWheel.py
+++ Kwarqs2012/source/components/ShooterWheel.py
@@ -1,12 +1,39 @@
+
+
+
 def Wheel(object):
     
     
-    __init__(self,mNumber)
-        self.currentSpeed = o
-        self.wheel = CANJaguar()
+    def __init__(self,mNumber):
+        self.currentSpeed = 0
+        self.goalSpeed = 0
+        self.wheel = CANJaguar(mNumber)
+               
+        
+    '''   
+        Description: sets variable that tells update to set the speed
+
+        Variables:    
+            float speed (-1 to 1) - speed to set the motor
+    '''
+    def SetSpeed(self,goalSpeed):
+        self.goalSpeed = goalSpeed
         
         
-    def SetSpeed(mNumber)
-         
-        self.wheel.SetSpeed = 0
+    #checks if current speed = goal speed
+    def IsReady(self):
+        if self.currentSpeed == self.goalSpeed:
+            return True
+    
+    
+    #update the current speed and goal speed
+    def Update(self):
+        self.wheel.Set(goalSpeed)
+        currentSpeed = self.wheel.GetSpeed()
         
+            
+            
+    '''RM gives # of goalSpeed,mNumber
+    mNumber never changes within an instance
+    goalSpeed gets updated in update
+    

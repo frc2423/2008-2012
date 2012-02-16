b941112a38d45b49244d2496848de2320db47de4
Dustin Spicuzza <dustin@virtualroadside.com>
2012-02-16 02:21:10 +0000

    Updated electrical test program to reflect actual motors and such
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@575 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/electrical_test/robot.py
+++ Kwarqs2012/electrical_test/robot.py
@@ -6,33 +6,53 @@
     functionality of the robot. By having something simple that *just works*,
     we can test the robot when it is having problems and determine
     if the problems are mechanical, electrical, or program-related. 
+    
+    Controls:
+    
+        Stick1:
+        
+            X/Y: drive motors
+            Trigger: ramp motor
+            
+            Buttons 10,11: relays
+            
+        Stick2:
+        
+            Trigger: shooter (z axis)
+            Buttons 8,9: lazy susan
+            Buttons 2,3: feeder
 
 '''
 
 
-import wpilib
+try:
+    import wpilib
+except ImportError:
+    import fake_wpilib as wpilib
 
 # our custom python module for tracking baskets
-from basket_tracker import BasketTracker
+#from basket_tracker import BasketTracker
 
 # TODO: Give these better names to reflect what they do
 # TODO: Add sensors as needed
 # TODO: Organize the motors and sensors better
 
 # Joysticks
-stick1 = Joystick(1)
-stick2 = Joystick(2)
+stick1 = wpilib.Joystick(1)
+stick2 = wpilib.Joystick(2)
 
 # PWM motor controllers (drive motors)
-motor_pwm1 = wpilib.Jaguar(1)
-motor_pwm2 = wpilib.Jaguar(2)
+l_motor = wpilib.Jaguar(2)
+r_motor = wpilib.Jaguar(1)
+feeder_motor = wpilib.Jaguar(3)
+#camera_servo = wpilib.Jaguar(5)
 
 # CAN motor controllers
-motor_can2 = wpilib.CANJaguar(2)
-motor_can3 = wpilib.CANJaguar(3)
-motor_can4 = wpilib.CANJaguar(4)
-motor_can5 = wpilib.CANJaguar(5)
-motor_can6 = wpilib.CANJaguar(6)
+shooter_motor1 = wpilib.CANJaguar(2)
+shooter_motor2 = wpilib.CANJaguar(3)
+angle_motor = wpilib.CANJaguar(4)
+ramp_motor = wpilib.CANJaguar(5)
+susan_motor = wpilib.CANJaguar(6)
 
 # relays
 relay1 = wpilib.Relay(1)
@@ -41,13 +61,14 @@ relay3 = wpilib.Relay(3)
 relay4 = wpilib.Relay(4)
 
 # vision basket tracking code
-tracker = BasketTracker()
+#tracker = BasketTracker()
 
 
 class MyRobot(wpilib.SimpleRobot):
 
     def __init__(self):
         wpilib.SimpleRobot.__init__(self)
+        self.drive = wpilib.RobotDrive( l_motor, r_motor )
 
     def Disabled(self):
         
@@ -81,24 +102,66 @@ class MyRobot(wpilib.SimpleRobot):
    
             dog.Feed()
             
-            # TODO: Add useful motor controls here
+            #
+            # Drive motors
+            #
             
-            motor_pwm1.Set( stick1.GetY() )
-            motor_pwm2.Set( stick1.GetY() )
+            self.drive.ArcadeDrive( stick1 )
             
-            motor_can2.Set( stick1.GetY() )
-            motor_can3.Set( stick1.GetY() )
-            motor_can4.Set( stick1.GetY() )
-            motor_can5.Set( stick1.GetY() )
-            motor_can6.Set( stick1.GetY() )
+            #
+            # Ramp motor
+            #
             
             if stick1.GetTrigger():
+                angle_motor.Set( 1.0 )
+            else:
+                angle_motor.Set( -1.0 )
+            
+            #
+            # Shooter motor
+            #
+            
+            if stick2.GetTrigger():
+                z = stick2.GetZ()
+                shooter_motor1.Set( z )
+                shooter_motor2.Set( -z )
+            else:
+                shooter_motor1.Set( 0.0 )
+                shooter_motor2.Set( 0.0 )
+            
+            #
+            # Feeder
+            #
+             
+            if stick2.GetRawButton(3):      # up
+                feeder_motor.Set( 1.0 )
+            elif stick2.GetRawButton(2):    # down
+                feeder_motor.Set( -1.0 )
+            else:
+                feeder_motor.Set( 0 )
+                
+            #
+            # Lazy susan
+            #
+            
+            if stick2.GetRawButton(8):      # left
+                susan_motor.Set( 1.0 )
+            elif stick2.GetRawButton(9):    # right
+                susan_motor.Set( -1.0 )
+            else:
+                susan_motor.Set( 0.0 )
+            
+            #
+            # Relays
+            #
+            
+            if stick1.GetRawButton(10):
                 relay1.Set( wpilib.Relay.kForward )
                 relay2.Set( wpilib.Relay.kForward )
                 relay3.Set( wpilib.Relay.kForward )
                 relay4.Set( wpilib.Relay.kForward )
             
-            elif stick1.GetTop():
+            elif stick1.GetRawButton(11):
                 relay1.Set( wpilib.Relay.kReverse )
                 relay2.Set( wpilib.Relay.kReverse )
                 relay3.Set( wpilib.Relay.kReverse )
@@ -124,7 +187,7 @@ def run():
     '''This function must be present for the robot to start'''
     robot = MyRobot()
     robot.StartCompetition()
-
+    return robot
 
 
 

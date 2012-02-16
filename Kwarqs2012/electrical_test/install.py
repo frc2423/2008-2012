75a2f38b713331f49ad45ba1d24ca59990eb81e5
Dustin Spicuzza <dustin@virtualroadside.com>
2012-02-16 05:08:41 +0000

    Added installer file
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@576 eb211378-2a44-443b-ae4a-d45323d45073

---
+++ Kwarqs2012/electrical_test/install.py
@@ -0,0 +1,37 @@
+
+import os
+import imp
+
+try:
+    installer = imp.load_source( 'install', 'C:\\WindRiver\\workspace\\robotpy\\utilities\\installer\\install.py' )
+except IOError, e:
+    print( "Error loading the RobotPy installer library. (%s)\n\n" % e +
+           "-- Is RobotPy source installed at 'C:\\WindRiver\\workspace\\robotpy'?" )
+    exit(1)
+    
+robot_host = '10.24.23.2'
+    
+print( 'Connecting to robot host %s' % robot_host )
+
+try:
+    server = installer.RobotCodeInstaller( robot_host )
+except Exception as e:
+    print( "Error connecting to remote host: %s" % e )
+    exit(1)
+
+local_root = os.path.dirname(__file__)
+remote_root = '/py'
+
+print( 'Beginning upload.' )
+
+# verify the directories actually exist
+server.create_remote_directory( remote_root )
+server.delete_remote( remote_root + '/__pycache__' )
+
+# upload the files
+server.upload_file( remote_root, local_root, 'robot.py' )
+
+print( 'Upload complete' )
+
+# close the installer
+server.close()

64f2ade528c95e5c40dd70485897e1f64f2dffe0
John Scudder <jscudderz@gmail.com>
2012-02-16 01:15:08 +0000

    made da thing
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@570 eb211378-2a44-443b-ae4a-d45323d45073

---
+++ Kwarqs2012/source/components/ShooterSusan.py
@@ -0,0 +1,28 @@
+def Susan(object):
+    
+    '''mNumber is CANJaguar ID'''
+    def __init__(self,mNumber,gyro)
+       self.mNumber = CANJaguar
+        self.CurrentAngle = 0
+        self.GoalAngle = 0
+        self.gyro = Gyro
+    
+    def turn(self,mNumber)
+        forwards = 1
+        off = 0
+        backwards = -1
+    
+        motor.set(forwards)
+        
+    def SetGoal(degrees)
+        self.GoalAngle = degrees
+    
+    
+    def IsReady(self)
+        if self.GoalAngle == self.CurrentAngle:
+            return True
+    
+    def Update(self)
+        self.mNumber.Set
+        self.gryo.SetAngle(self.GoalAngle)
+        motor.set(forwards)

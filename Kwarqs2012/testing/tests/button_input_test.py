98852dc2889dd873b240a421809a02c973a5bf0c
Jeff Conde <jsconde@comcast.net>
2012-02-18 17:15:56 +0000

    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@625 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/testing/tests/button_input_test.py
+++ Kwarqs2012/testing/tests/button_input_test.py
@@ -1,11 +1,35 @@
-
+try:
+    import wpilib
+except ImportError:
+    import fake_wpilib as wpilib
 # directory of robot.py, relative to this file
+import robot
 robot_path = '../../source'
 
 
 def run_tests( robot_module, myrobot ):
 
     # TODO: Actually do something here
+    loop_count = 0
+    
+    while loop_count < 10:
+        loop_count += 1
+        
+        if loop_count == 1:
+            wpilib.self.buttons[0]
+        
+         if loop_count == 2:
+            wpilib.self.buttons[1]
+        
+         if loop_count == 3:
+            wpilib.self.buttons[4]
+        
+         if loop_count == 4:
+            wpilib.self.buttons[5]
+        
+         if loop_count == 5:
+            wpilib.self.buttons[6]
+    
 
-    pass
+   
 

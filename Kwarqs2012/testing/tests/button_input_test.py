dcbc61c83c99b11dbeb3a094f44e116b0b0b3cea
Jeff Conde <jsconde@comcast.net>
2012-02-18 18:26:45 +0000

    Changing test program to a class
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@627 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/testing/tests/button_input_test.py
+++ Kwarqs2012/testing/tests/button_input_test.py
@@ -3,33 +3,56 @@ try:
 except ImportError:
     import fake_wpilib as wpilib
 # directory of robot.py, relative to this file
-import robot
+
+
 robot_path = '../../source'
 
 
-def run_tests( robot_module, myrobot ):
+class Test(object):
+    def __init__(self):
+        self.loop_count = 0
 
-    # TODO: Actually do something here
-    loop_count = 0
-    
-    while loop_count < 10:
-        loop_count += 1
+    def IsOperatorControl(self):
+        #global loop_count
+        self.loop_count = loop_count + 1
+        print(self.loop_count)
         
-        if loop_count == 1:
-            wpilib.self.buttons[0]
-        
-         if loop_count == 2:
-            wpilib.self.buttons[1]
-        
-         if loop_count == 3:
-            wpilib.self.buttons[4]
-        
-         if loop_count == 4:
-            wpilib.self.buttons[5]
-        
-         if loop_count == 5:
-            wpilib.self.buttons[6]
-    
+        if self.loop_count == 10:
+            return False
+            
+        return True
 
-   
+
+    def run_tests( self, robot_module, myrobot ):
+
+        myrobot.enabled = True
+        myrobot.on_IsOperatorControl = IsOperatorControl
+        
+        myrobot.OperatorControl()
+
+        # TODO: Actually do something here
+        '''
+        loop_count = 0
+        
+        while loop_count < 10:
+            loop_count += 1
+            
+            if loop_count == 1:
+                wpilib.Joystick.GetRawButton(1)
+            
+            if loop_count == 2:
+                wpilib.buttons[1]
+            
+            if loop_count == 3:
+                wpilib.self.buttons[4]
+            
+            if loop_count == 4:
+                wpilib.self.buttons[5]
+            
+            if loop_count == 5:
+                wpilib.self.buttons[6]
+        '''
+            
+
+       
 

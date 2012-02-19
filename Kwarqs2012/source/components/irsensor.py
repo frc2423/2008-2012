50adeef703f367bf1690022f19e8e7390c3dde52
Sam Rosenblum <textgame@gmail.com>
2012-02-19 18:16:44 +0000

    Added irsensor.py
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@640 eb211378-2a44-443b-ae4a-d45323d45073

---
+++ Kwarqs2012/source/components/irsensor.py
@@ -0,0 +1,39 @@
+'''
+    Defines the functionality of the analog irsensors used to detect
+'''
+try:
+    import wpilib
+except ImportError:
+    import fake_wpilib as wpilib
+    
+class IRSensor(object):
+    
+    '''
+        values refers to distance based on schmatic of
+        sharp GP2D12 and size of ball being ~10cm,
+        should be verified imperically, sensors offset by
+        5cm from ball
+    '''
+    #by schematic, refers to sensor reporting ~14cm distance
+    inditThresh = .9 
+    #by schematic, refers to sensor reporting ~6cm distance
+    ballSet = 2 
+    
+    def __init__(self, irSensor):
+        self.irSensor = wpilib.AnalogChannel(irSensor)
+        self.state = False
+        
+
+        
+    '''
+        returns the current state of the ball, if a threshold is
+        not passed then do not change the state
+    '''
+    def isBallSet(self):
+        voltage = self.irSensor.getVoltage()
+        if voltage < IRSensor.inditThresh:
+            self.state = False
+        elif  voltage > IRSensor.ballSet:
+            self.state = True
+        return self.state    
+        

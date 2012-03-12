61f676738234176f154373eba21b404773b720ca
Dustin Spicuzza <dustin@virtualroadside.com>
2012-03-12 07:03:00 +0000

    Fixes and changes to susan, wheel - Removed PID shooter, made it more difficult to find the necessary constants
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@736 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/source/components/shooter/susan.py
+++ Kwarqs2012/source/components/shooter/susan.py
@@ -1,68 +1,136 @@
-from components.shooter.pidshooter import SusanSource
-from ez_can_jaguar import EzCANJaguar
+
 try:
     import wpilib
 except ImportError:
     import fake_wpilib as wpilib
 
+import threading
+    
 
+class SusanSource(wpilib.PIDSource):
+    '''Source for the Susan PID controller'''
+    
+    def __init__(self):
+        wpilib.PIDSource.__init__(self)
+        self._lock = threading.RLock()
+        self.val = 0.0
+    
+    def Set(self, val):
+        with self._lock:
+            self.val = val
+    
+    def PIDGet(self):
+        with self._lock:
+            return self.val
+    
+    
+class SusanPidOutput(wpilib.PIDOutput):
+    '''Jaguar output for susan PID'''
+    
+    def __init__(self, jaguar):
+        wpilib.PIDOutput.__init__(self)
+        self.jaguar = jaguar
+
+    def PIDWrite(self, output):
+        self.jaguar.Set(output)
+        
+    
 
 class Susan(object):
+    '''
+        The top of our robot has a shooter, which is mounted on a lazy
+        susan. There is a motor attached to the susan which allows it 
+        to turn *almost* 360 degrees. Its like.. 320 or something. 
+        
+        TODO: We don't handle the case where the susan reaches the end
+        of its limit switches in automated mode
+        
+        TODO: Need to figure out semantics for gyro-based angle control
+    '''
+    
     
     SUSAN_P = 1.0
     SUSAN_I = 0.0
     SUSAN_D = 0.0
     SUSAN_CAMERA_MAX = 20
     SUSAN_MAX_SPEED = 1
-    ''' the percent tolerance of the angle to be considered ready'''
-    ANGLETOLERANCE = 5
+    ANGLETOLERANCE = 5      # the percent tolerance of the angle to be considered ready'''
     
-    def __init__(self,susanCAN,susanGyro, bodyGyro):
+    def __init__(self, susanCAN, susanGyro, bodyGyro):
         
-        self.susanMotor = EzCANJaguar(susanCAN)
+        self.susanMotor = wpilib.CANJaguar(susanCAN)
+        
+        # problem: when you initialize a gyro, it sits there for 6 seconds
+        # and doesn't let the robot do anything. How do we deal with this
+        # problem? 
         #self.gyro = wpilib.Gyro(susanGyro)
         #self.bodyGyro = wpilib.Gyro(bodyGyro)
         
         #It seems that you forgot to put a self to the right
         #self.bodyGyroInitPos = self.bodyGyro.GetAngle()
-        self.susanSource = SusanSource()
+        self.pid_source = SusanSource()
+        self.pid_output = SusanPidOutput(self.susanMotor)
         
-        self.pidControl = wpilib.PIDController(Susan.SUSAN_P, Susan.SUSAN_I, Susan.SUSAN_D, self.susanSource, self.susanMotor)
+        self.pidControl = wpilib.PIDController(Susan.SUSAN_P, Susan.SUSAN_I, Susan.SUSAN_D, self.pid_source, self.pid_output)
         self.pidControl.SetInputRange(-(Susan.SUSAN_CAMERA_MAX),Susan.SUSAN_CAMERA_MAX)
         self.pidControl.SetOutputRange(-(Susan.SUSAN_MAX_SPEED), Susan.SUSAN_MAX_SPEED)
         self.pidControl.SetTolerance(Susan.ANGLETOLERANCE)
         
-        self.goalAngle = 0
-        self.vBus = 0
-        self.autoMode = True
-                    
-    '''set goal Angle variable'''
-    def SetGoalAngle(self,angle):
-        self.goalAngle = angle
+        # this never changes, when we're using PID control the goal is to get
+        # the offset to zero
+        self.pidControl.SetSetpoint(0.0)
         
-    def SetVBus(self, vBus):
+        # warning: never enable the pid controller here
+        
+        self.angle = None
+        self.vBus = None
+        self.autoMode = False
+                    
+    
+    def SetAutoRelativeAngle(self,angle):
+        '''When called, this tells the susan to automatically go to an angle
+           relative to where the susan is currently pointing'''
+        self.angle = angle
+        
+        # TODO: If we use a gyro, this is no longer correct
+        
+    def SetVBus(self, value):
+        '''Manually control the susan. value is [-1,1]'''
         self.vBus = vBus
         
-    def SetMode(self, Auto):
-        pass
     
-    '''
-        start pointing at wall temporary return true 
-    '''
     def PointingCorrectly(self):
+        '''
+            start pointing at wall temporary return true 
+        '''
+        
+        # if there was a gyro here, we would use this to figure it out
+    
         return True 
             
-    '''
-        define ready when the susans angle is very near the goal angle
-        and we are pointing Correctly
-    '''
+    
     def IsReady(self):
+        '''
+            Returns True when:
+
+                - automode is off
+                - or the susan angle is very near the goal angle and 
+                the susan is pointing in the direction of the goal
+        '''
+        
+        if self.autoMode == False:
+            return True
+        
         return self.PointingCorrectly() and self.pidControl.OnTarget()
+        
     
     def Print(self):
-        print("ShooterSusan: ")
-        print("    Susan pVoltage:  "+ str(self.pidControl.Get()) + "  Susan GoalAngle: " + str(self.goalAngle) + "  Pointing Correctly: " + str(self.PointingCorrectly()))
-        print("    AutoMode: " + str(self.autoMode) + "  Susan vBus: " + str(self.vBus))
+        # TODO: Implement this when there is a unified format to print this.. 
+        pass
+        
+        #print("ShooterSusan: ")
+        #print("    Susan pVoltage:  "+ str(self.pidControl.Get()) + "  Susan GoalAngle: " + str(self.goalAngle) + "  Pointing Correctly: " + str(self.PointingCorrectly()))
+        #print("    AutoMode: " + str(self.autoMode) + "  Susan vBus: " + str(self.vBus))
     
     '''updates variables'''
     def Update(self):
@@ -72,12 +140,12 @@ class Susan(object):
         #if you set PVBus go into manual
         if self.vBus is not None:
             autoMode = False
-        if self.SetGoalAngle is not None:
+        if self.angle is not None:
             autoMode = True
         else:
             autoMode = False
        
-        #detect transitions 
+        #detect transitions to/from automode
         if autoMode != self.autoMode:
             if not autoMode:
                 self.pidControl.Disable()
@@ -85,19 +153,21 @@ class Susan(object):
                 enable_automode = True
             self.autoMode = autoMode
             
-            if self.autoMode:
-                ''' we always try to get the goalAngle to zero '''
-                self.pidControl.SetSetpoint(0)
-            
-            self.susanSource.setValue(self.goalAngle)
-
-        else:
-            self.susanMotor.Set(self.vBus)
-            
-        if enable_automode:
-            self.pidControl.Enable 
         
-        #reset Variables   
-        self.goalAngle = None
+        if self.autoMode:
+            self.pid_source.Set( self.angle )
+            
+        else:
+            if self.vBus is None:
+                self.vBus = 0
+        
+            self.susanMotor.Set(self.vBus)
+        
+        # do this last otherwise the PID control might be enabled with a nonsense goal
+        if enable_automode:
+            self.pidControl.Enable()
+        
+        #reset variables   
+        self.angle = None
         self.vBus = None
         
--- Kwarqs2012/source/components/shooter/wheel.py
+++ Kwarqs2012/source/components/shooter/wheel.py
@@ -1,102 +1,158 @@
 
-from components.shooter.pidshooter import ShooterWheelOutput
-from ez_can_jaguar import EzCANJaguar
-
 try:
     import wpilib
+    import wpilib.SmartDashboard
 except ImportError:
     import fake_wpilib as wpilib
+    import fake_wpilib.SmartDashboard
     
+import threading
+
     
-class EncoderShim(wpilib.PIDSource):
+class WheelPidSource(wpilib.PIDSource):
+    '''Encoder input for wheel PID'''
+
     def __init__(self, encoder):
         wpilib.PIDSource.__init__(self)
+        self._lock = threading.RLock()
         self.encoder = encoder
+        self.value = 0.0
+        
+    def Get(self):
+        with self._lock:
+            return self.value
         
     def PIDGet(self):
-        return self.encoder.PIDGet() 
+        value = self.encoder.PIDGet()    
+        with self._lock:
+            self.value = value
+        return value
 
+ 
+class WheelPidOutput(wpilib.PIDOutput):
+    '''Jaguar output for wheel PID'''
+    
+    SYNCGROUP = 2
+    
+    def __init__(self, wheelMotor1, wheelMotor2):
+        wpilib.PIDOutput.__init__(self)
+        
+        self._lock = threading.RLock()
+        self.wheelMotor1 = wheelMotor1
+        self.wheelMotor2 = wheelMotor2
+        
+        self.desiredPVBus = 0
+    
+    def Get(self):
+        with self._lock:
+            return self.value
+    
+    def Reset(self):
+        '''Always call this before enabling the PID controller'''
+        value = self.wheelMotor1.Get()
+    
+        with self._lock:
+            self.value = value
+    
+    def PIDWrite(self, output):
+    
+        with self._lock:
+            self.value += output
+            value = self.value
+    
+        self.wheelMotor1.Set(value, WheelPidOutput.SYNCGROUP)
+        self.wheelMotor2.Set(-value, WheelPidOutput.SYNCGROUP)
+        wpilib.CANJaguar.UpdateSyncGroup(WheelPidOutput.SYNCGROUP)
+
+        
          
 class Wheel(object):
-    
-    ENCODER_TURNS_PER_REVOLUTION = 360
-    ENCODERTOWHEELRATIO = 4
-    WHEEL_FULL_ROTATION = ENCODER_TURNS_PER_REVOLUTION * ENCODERTOWHEELRATIO
-    SHOOTER_MOTOR_P = 0.000675
-    SHOOTER_MOTOR_I = 0.000000050
-    SHOOTER_MOTOR_D = 0.01
-    MAXVPercent = 1
-    ''' percent tolerance'''
-    TOLERANCE = 1
-    
-    def __init__(self,wheelCAN1, wheelCAN2, encoder_tuple):
-        self.goalRPM = None
-        self.vBus = None
+    '''
+        The robot has a wheel on top of it used to shoot balls. We have an
+        encoder attached to it that allows us to (ideally) precisely control
+        the speed of the motor. 
         
-        self.wheelMotor1 = EzCANJaguar(wheelCAN1)
-        self.wheelMotor2 = EzCANJaguar(wheelCAN2)
+        If we can get the PID stuff to work really well, there really isn't
+        any reason the user should ever use Vbus mode
+    '''
+    
+    # used to determine the rate
+    ENCODER_SINGLE_ROTATION = 16
+    
+    # PID constants
+    MOTOR_P = 0.000675
+    MOTOR_I = 0.000000050
+    MOTOR_D = 0.01
+    
+    ''' percent tolerance'''
+    PID_TOLERANCE = 1
+    
+    def __init__(self, wheelCAN1, wheelCAN2, encoder_tuple):
+    
+        self.vBus = None
+        self.auto_speed = None
+        self.autoMode = False
+        
+        self.wheelMotor1 = wpilib.CANJaguar(wheelCAN1)
+        self.wheelMotor2 = wpilib.CANJaguar(wheelCAN2)
         
         self.wheelMotor1.ConfigNeutralMode( wpilib.CANJaguar.kNeutralMode_Coast )
         self.wheelMotor2.ConfigNeutralMode( wpilib.CANJaguar.kNeutralMode_Coast )
         
-        self.shooterWheelOutput = ShooterWheelOutput(self.wheelMotor1, self.wheelMotor2)
-        
-        self.autoMode = False
-        
         self.encoder = wpilib.Encoder(encoder_tuple[0],encoder_tuple[1])
         self.encoder.SetPIDSourceParameter(wpilib.Encoder.kRate)
-        self.encoder.SetDistancePerPulse(1.0 / 16.0)
+        self.encoder.SetDistancePerPulse(1.0 / Wheel.ENCODER_SINGLE_ROTATION)
         self.encoder.Start()
         
-        self.encoderShim = EncoderShim(self.encoder)
+        # setup PID stuff
         
-        self.pidControl = wpilib.PIDController(Wheel.SHOOTER_MOTOR_P, Wheel.SHOOTER_MOTOR_I, Wheel.SHOOTER_MOTOR_D, self.encoderShim,  self.shooterWheelOutput)
-        #self.pidControl.SetOutputRange(-(Wheel.MAXVPercent), Wheel.MAXVPercent)
+        self.pid_source = WheelPidSource(self.encoder)
+        self.pid_output = WheelPidOutput(self.wheelMotor1, self.wheelMotor2)
         
-        # dont enable the PID controller here
-
-            
-    '''   
-        Description: sets variable that tells update to set the speed
-
-        Variables:    
-            float speed (-1 to 1) - speed to set the motor
-    '''
-    def SetRPM(self,goalRPM):
-        self.goalRPM = goalRPM
+        self.pid_controller = wpilib.SmartDashboard.SendablePIDController(Wheel.MOTOR_P, Wheel.MOTOR_I, Wheel.MOTOR_D, self.pid_source, self.pid_output)
+        self.pid_controller.SetTolerance( Wheel.PID_TOLERANCE )
     
+        # warning: dont enable the PID controller here
+
+        # SmartDashboard stuff
+        
+        sd = wpilib.SmartDashboard.SmartDashboard.GetInstance()
+        sd.PutData( "Wheel PID", self.pid )
+
+    
+    def IsReady(self):
+        '''Returns True if the shooter is ready for a ball to be fed into it'''
+        if self.autoMode == False:
+            return True
+        
+        return self.pid_controller.OnTarget()
+        
+    
+    def SetAutoSpeed(self, speed):
+        '''This is a value between 0.0 and 99.0 indicating the desired speed
+           of the wheel. This speed will be attained using PID control'''
+        self.auto_speed = speed
     
             
     def SetVBus(self, vBus):
         ''' Set motor vbus directly (-1 to 1) '''
         self.vBus = vBus
         
-    
-    def IsReady(self):
-        '''Returns True if the shooter is ready for a ball to be fed into it'''
-        if self.autoMode == False:
-            return True
-            
-        # TODO: Not sure if this is actually done right
-        return self.pidControl.OnTarget()
         
-    def SetMode(self, Auto):
-        pass
-        
-            
     def Print(self):
         print( "ShooterWheel:")
-        print( "    Wheel RPM: %f; Goal RPM: %s; Auto Mode: %s" % (self.encoder.GetRate(), self.goalRPM, self.autoMode))
+        print( "    Wheel RPM: %f; Set Speed: %s; Auto Mode: %s" % (self.encoder.GetRate(), self.auto_speed, self.autoMode))
         
-    '''update the current speed and goal speed'''
+    
     def Update(self):
+        '''Actually do stuff here'''
     
         enable_automode = False
     
         # if you set vbus, you win
         if self.vBus is not None:
             autoMode = False
-        if self.goalRPM is not None:
+        if self.auto_speed is not None:
             autoMode = True
         else:
             autoMode = False
@@ -104,13 +160,13 @@ class Wheel(object):
         # detect transitions to/from automode
         if autoMode != self.autoMode:
             if not autoMode:
-                self.pidControl.Disable()
+                self.pid_controller.Disable()
             else:
                 enable_automode = True
             self.autoMode = autoMode
     
         if self.autoMode:
-            self.pidControl.SetSetpoint(self.goalRPM)
+            self.pid_controller.SetSetpoint(self.auto_speed)
         else:
             if self.vBus is None:
                 self.vBus = 0
@@ -119,16 +175,12 @@ class Wheel(object):
             self.wheelMotor2.Set(-self.vBus, ShooterWheelOutput.SYNCGROUP)
             wpilib.CANJaguar.UpdateSyncGroup(ShooterWheelOutput.SYNCGROUP)
             
+        # do this last otherwise the PID control might be enabled with a nonsense setpoint
         if enable_automode:
-            self.pidControl.Enable()
+            self.pid_output.Reset()
+            self.pid_controller.Enable()
             
         # reset vars at the end
-        self.goalRPM = None
+        self.auto_speed = None
         self.vBus = None
-            
-            
-    '''
-        RM gives # of goalSpeed,mNumber
-        mNumber never changes within an instance
-        goalSpeed gets updated in update
-    '''
+        

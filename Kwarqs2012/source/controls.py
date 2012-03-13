57188fa17e6b3c54c7ff3c92bb25e7873f13522f
Dustin Spicuzza <dustin@virtualroadside.com>
2012-03-13 08:01:23 +0000

    Added main logic and controls, not sure I like it yet but it seems to run for now. More testing required.
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@759 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/source/controls.py
+++ Kwarqs2012/source/controls.py
@@ -3,63 +3,7 @@
 This file defines the mapping of various controls here, so that way to change
 way things are mapped, we don't have to change the actual logic or anything.
 
-
-Stick1:
-
-    X/Y axis drives robot
-    Trigger: smoother turning
-    
-    6/11: Ramp arm down
-    
-    Z axis: manual camera angle
-    Top button: enable manual camera pointing
-    
-Stick2:
-
-    Shooter:
-        Automated:
-            Trigger: fire ball (move ball from chamber)
-            
-        Manual:
-            Trigger: activate wheel
-            Z axis: manual wheel speed
-        
-            6: move ball from chamber into shooter
-            
-    
-    Susan:
-        Automated:
-            None
-    
-        Manual:
-            Top button: activate manual lazy susan
-            X axis: turn lazy susan
-                
-            
-    Ball Handling:
-        Continuous feeding:
-            None
-    
-        Automated:
-            10/11: activate feeding
-        
-        Manual:
-            6/7: manual chamber 
-            10/11: manual feeder
-    
-        
-Digital inputs (switches):
-    
-    Auto distance enable/disable
-    Auto susan enable/disable
-    Auto shoot enable/disable
-    Auto ball handling enable/disable
-    Continuous feeding enable/disable
-        -> perhaps this should be on the dashboard?
-
-
 '''
-
 
 try:
     import wpilib
@@ -72,31 +16,47 @@ Y = wpilib.Joystick.kDefaultYAxis
 Z = wpilib.Joystick.kDefaultZAxis
 
 # button constants
-TRIGGER = wpilib.kDefaultTriggerButton      # 1
-TOP = wpilib.kDefaultTopButton              # 2
+TRIGGER = wpilib.Joystick.kDefaultTriggerButton      # 1
+TOP = wpilib.Joystick.kDefaultTopButton              # 2
 
 # axis definitions -- a tuple of (stick number, axis)
 # -> call GetJoystickAxis() with this value to get the axis
 
-DRIVE_X_AXIS = (1, X)
-DRIVE_Y_AXIS = (1, Y)
+DRIVE_X_AXIS        = (1, X)
+DRIVE_Y_AXIS        = (1, Y)
+MANUAL_CAMERA_AXIS  = (1, Z)
 
-MANUAL_SUSAN_SPEED_AXIS = (2, X)
-MANUAL_WHEEL_SPEED_AXIS = (2, Z)
+SUSAN_TURN_AXIS     = (2, X)
+SHOOTER_WHEEL_AXIS  = (2, Z)
 
 
 # button definitions -- (stick number, [list of buttons])
 # -> call StickButtonPressed() with this value to get True/False
 
-AUTOMATIC_SHOOTER_FIRE_BUTTON   = (2, TRIGGER)
-MANUAL_SHOOTER_WHEEL_BUTTON     = (2, TRIGGER)
-MANUAL_SHOOTER
+RAMP_ARM_DOWN_BUTTON = (1,6)
+MANUAL_CAMERA_ENABLE_BUTTON = (1, TOP)
+
+SUSAN_TURN_BUTTON = (2, TOP)
+
+BALL_HANDLER_MANUAL_CHAMBER_UP_BUTTON   = (2, 6)
+BALL_HANDLER_MANUAL_CHAMBER_DOWN_BUTTON = (2, 7)
+BALL_HANDLER_MANUAL_FEEDER_FEED_BUTTON  = (2, 11)
+BALL_HANDLER_MANUAL_FEEDER_EXPEL_BUTTON = (2, 10)
+
+SHOOT_BALL_BUTTON = (2, TRIGGER)
+
+DEBUG_BUTTON = (2, 8)
 
 
 # switch definitions
 # -> call GetSwitch() with this value to get True/False
 
-AUTO_DISTANCE_SWITCH
+# TODO: Fix these
+MANUAL_SHOOTER_WHEEL_ENABLE_SWITCH  = 0
+AUTO_BALL_HANDLER_SWITCH            = 1
+CONTINUOUS_BALL_HANDLER_SWITCH      = 2
+AUTO_SUSAN_SWITCH                   = 3
+AUTO_DISTANCE_SWITCH                = 4
 
 #
 # Utility functions
@@ -109,10 +69,10 @@ ds = wpilib.DriverStation.GetInstance()
 def GetJoystickAxis( tuple ):
     return ds.GetStickAxis( tuple[0], tuple[1] )
 
-def StickButtonPressed( tuple ):
-    for 
-    return ds.GetStickButtons
+def StickButtonOn( tuple ):
+    return ds.GetStickButtons( tuple[0] ) & (1 << tuple[1])
 
 def SwitchOn( val ):
-    return 
+    # TODO
+    return True
  
--- Kwarqs2012/source/robot.py
+++ Kwarqs2012/source/robot.py
@@ -22,23 +22,21 @@ try:
 except ImportError:
     import fake_wpilib as wpilib
    
-
-from robot_manager import RobotManager   
-from util import PrintTimer
-
+from autonomous import AutonomousModeManager
 from controls import *
+from operator_leds import OperatorLEDs
+from util import ConsoleLED, PrintTimer
 
 
 #
 # Import all components here
 #
 
-from components.ball_handler.chamber import Chamber
-from components.ball_handler.feeder import Feeder
-from components.shooter import Shooter
-from components.shooter.susan import Susan
-from components.shooter.wheel import Wheel 
-from components.ramp_arm import RampArm
+from components import RobotManager, RampArm
+from components.ball_handler import BallHandler, Chamber, Feeder
+from components.shooter import Camera, Susan, Wheel
+
+
 
 #
 # These are all the ports and identifiers needed to initialize various
@@ -46,12 +44,14 @@ from components.ramp_arm import RampArm
 #
         
 #Relay Inputs
-chamberRelay = 2
+chamberRelay    = 2
+cameraRelay     = 3
 
 # PWM Jaguars
 l_motor_pwm_ch  = 2
 r_motor_pwm_ch  = 1
 feeder_pwm_ch   = 3
+camera_servo_ch = 5
         
 #Digital Channel Inputs
 botFeedSwitch   = 1
@@ -70,24 +70,31 @@ angleCAN        = 4
 rampArmCAN      = 5
 susanCAN        = 6
 
+# console LED
+consoleLedPort  = 10
+
 #
 # Create instances of all components here
 #
 
-chamber = Chamber(chamberRelay, chambIRSens)
 
-feeder = Feeder(feeder_pwm_ch, botFeedSwitch, topFeedIRSens)
 
-wheel = Wheel(shootWheelCAN1, shootWheelCAN2, wheelEncoder)
-susan = Susan(susanCAN, susanGyro, bodyGyro)
-shooter = Shooter(susan, wheel)
+chamber         = Chamber(chamberRelay, chambIRSens)
+feeder          = Feeder(feeder_pwm_ch, botFeedSwitch, topFeedIRSens)
+ballHandler     = BallHandler(chamber, feeder)
 
-rampArm = RampArm(rampArmCAN)
+camera          = Camera(camera_servo_ch, cameraRelay)
+susan           = Susan(susanCAN, susanGyro, bodyGyro)
+wheel           = Wheel(shootWheelCAN1, shootWheelCAN2, wheelEncoder)
 
-robotManager = RobotManager(chamber, feeder, wheel, susan, shooter)
+rampArm         = RampArm(rampArmCAN)
 
-l_driveMotor = wpilib.Jaguar(l_motor_pwm_ch)
-r_driveMotor = wpilib.Jaguar(l_motor_pwm_ch)
+consoleLed      = ConsoleLED(consoleLedPort)
+operatorLeds    = OperatorLEDs()
+robotManager    = RobotManager(ballHandler, camera, wheel, susan, consoleLed)
+
+l_driveMotor    = wpilib.Jaguar(l_motor_pwm_ch)
+r_driveMotor    = wpilib.Jaguar(r_motor_pwm_ch)
 
 
 drive = wpilib.RobotDrive(l_driveMotor, r_driveMotor)
@@ -127,7 +134,9 @@ class MyRobot(wpilib.SimpleRobot):
         self.ds = wpilib.DriverStation.GetInstance()
         self.print_timer = PrintTimer()
         
-        self.autonomous_manager = AutonomousManager(ds, drive, rampArm, ballHandler, shooter, robotManager)
+        self.autonomous_manager = AutonomousModeManager(ds, drive, rampArm, 
+                                                        ballHandler, robotManager,
+                                                        wheel )
     
     def Disabled(self):
         
@@ -232,78 +241,63 @@ class MyRobot(wpilib.SimpleRobot):
             
             dog.Feed()
             
-            # get the joystick buttons and axis here, so
-            # that we have a consistent state the entire time
-            
-            j1_x
-            j1_y
-            j1_z
-            j1_b
-            
-            j2_x
-            j2_y
-            j2_z = self.ds.
-            j2_b
-            
-            
-            
-            
-            drive.ArcadeDrive(stick1.GetY(), stick1.GetX())
-           
-            ###############################################
-            # state 
-            ###############################################
-           
-            auto_shooter = SwitchOn( AUTOSHOOTER_ENABLE_SWITCH )
+            drive.ArcadeDrive(GetJoystickAxis(DRIVE_Y_AXIS), GetJoystickAxis(DRIVE_X_AXIS))
             
             
             ###############################################
             # RampArm
             ###############################################
-            
-           
-            ###############################################
-            # Shooter
-            ###############################################
-           
+    
             try:
-                if auto_shooter:
-                    
-                    # notional, not sure if this is how to do this
-                    shooter.AutoShoot()
-                
-                    if StickButtonPressed( AUTOSHOOTER_FIRE_BUTTON ):
-                        shooter.Fire()
-                    
-                else:
-                    
-                    # manual shooter operation
-                    
-                    if StickButtonPressed( SHOOTER_WHEEL_BUTTON ):
-                    
-                        z = GetJoystickAxis( SHOOTER_WHEEL_AXIS )
-                        z = ((z * -1.0) + 1.0) / 2.0
-                    
-                        shooter.SpinWheel( z )
-                        
-                        
-                    if StickButtonPressed( BALL_HANDLER_
-                        
-                        ball_handler.FeedShooter()
-                    
+                if StickButtonOn( RAMP_ARM_DOWN_BUTTON ):
+                    rampArm.Extend()
             except:
                 if not self.ds.IsFMSAttached():
                     raise
+            
            
+            ###############################################
+            # Manual Shooter
+            ###############################################
+            
+            try:
+                if SwitchOn( MANUAL_SHOOTER_WHEEL_ENABLE_SWITCH ):
+                
+                    z = GetJoystickAxis( SHOOTER_WHEEL_AXIS )
+                    z = ((z * -1.0) + 1.0) * 50.0
+                
+                    wheel.SetAutoSpeed( z )
+            
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise
+            
+            
+            ###############################################
+            # Camera
+            ###############################################            
+            
+            try:
+                if StickButtonOn( MANUAL_CAMERA_ENABLE_BUTTON ):
+                
+                    # TODO: not sure what the appropriate angles are for this
+                    angle = GetJoystickAxis( MANUAL_CAMERA_AXIS )
+                    angle = ((angle * -1.0) + 1.0) * 90.0
+                
+                    camera.SetAngle( angle )
+                
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise    
+                    
            
             ###############################################
             # Susan
             ###############################################
            
             try:
-            
-                if StickButtonPressed( SUSAN_TURN_BUTTON ):
-                    susan.Turn( GetJoystickAxis( SUSAN_TURN_AXIS ) )
+                if StickButtonOn( SUSAN_TURN_BUTTON ):
+                    susan.SetVBus( GetJoystickAxis( SUSAN_TURN_AXIS ) )
                 
             except:
                 if not self.ds.IsFMSAttached():
@@ -315,32 +309,37 @@ class MyRobot(wpilib.SimpleRobot):
             ###############################################
            
             #
-            # TODO: this is a bit disjoint, there is some overlap
-            # here that this model is missing... 
+            # Enable automated behaviors if desired
             #
            
             try:
-                if SwitchOn( BALL_HANDLER_CONTINUOUS ):
+                if SwitchOn( AUTO_BALL_HANDLER_SWITCH ):
+                    ballHandler.AutoFeed()
+            
+                if SwitchOn( CONTINUOUS_BALL_HANDLER_SWITCH ):
+                    ballHandler.ContinuousFeed()
+                
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise
+
+            #
+            # Always allow manual control
+            #
+             
+            try:
+                if StickButtonOn( BALL_HANDLER_MANUAL_CHAMBER_UP_BUTTON ):
+                    chamber.Run()
                     
-                if SwitchOn( BALL_HANDLER_AUTOMATED ):
+                elif StickButtonOn( BALL_HANDLER_MANUAL_CHAMBER_DOWN_BUTTON ):
+                    chamber.Remove()
+                
+            
+                if StickButtonOn( BALL_HANDLER_MANUAL_FEEDER_FEED_BUTTON ):
+                    feeder.Feed()
                     
-                    if 
-                    
-                else:
-                    # complete manual control over ball handling
-                    
-                    if StickButtonsPressed( BALL_HANDLER_MANUAL_CHAMBER_UP_BUTTONS ):
-                        chamber.RunBeltUp()
-                        
-                    elif StickButtonsPressed( BALL_HANDLER_MANUAL_CHAMBER_DOWN_BUTTONS ):
-                        chamber.RunBeltDown()
-                    
-                    
-                    if StickButtonsPressed( BALL_HANDLER_MANUAL_FEEDER_FEED_BUTTONS ):
-                        feeder.Feed()
-                        
-                    elif StickButtonsPressed( BALL_HANDLER_MANUAL_FEEDER_EXPEL_BUTTONS ):
-                        feeder.Expel()
+                elif StickButtonOn( BALL_HANDLER_MANUAL_FEEDER_EXPEL_BUTTON ):
+                    feeder.Expel()
                     
             except:
                 if not self.ds.IsFMSAttached():
@@ -348,15 +347,43 @@ class MyRobot(wpilib.SimpleRobot):
            
            
             ###############################################
+            # Robot Manager
+            ###############################################
+           
+            try:
+                if SwitchOn( AUTO_SUSAN_SWITCH ):
+                    robotManager.EnableAutoSusan()     
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise
+                    
+            try:
+                if SwitchOn( AUTO_DISTANCE_SWITCH ):
+                    robotManager.EnableAutoDistance()     
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise
+           
+            try:
+                if StickButtonOn( SHOOT_BALL_BUTTON ):
+                    robotManager.ShootBall()
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise
+            
+           
+            ###############################################
             # Debug support
             ###############################################
            
+            #
+            # Don't enable debug output on the field, you can't
+            # see it there anyways, and we might cause an exception
+            #
             
-            # don't enable debug output on the field, you can't
-            # see it there anyways
             if not self.ds.IsFMSAttached():
             
-                if StickButtonPressed( DEBUG_BUTTON ) and self.print_timer.should_print():
+                if StickButtonOn( DEBUG_BUTTON ) and self.print_timer.should_print():
                     feeder.Print()
                     print("")
                     chamber.Print()
@@ -369,29 +396,94 @@ class MyRobot(wpilib.SimpleRobot):
             self._Update()
             
             # wait for more user input
-            wpilib.Wait(0.04)
+            wpilib.Wait(0.02)
             
             
     def _Update(self):
         '''
             Run the update functions for all components. Called from
             OperatorControl and Autonomous modes
+            
+            These should be done in a particular order! We want to call 
+            the high level abstractions first, followed by the low level
+            classes last.
+            
+            Update functions are ONLY called from here, so we don't 
+            accidentally call it twice from somewhere (which could
+            cause unexpected results!!)
         '''
         
-        try:
-            rampArm.Update()    
-        except:
-            if not self.ds.IsFMSAttached():
-                raise
+        # high level stuff first
         
         try:
             robotManager.Update()
         except:
             if not self.ds.IsFMSAttached():
                 raise
-    
-            
                 
+        try:
+            ballHandler.Update()
+        except:
+            if not self.ds.IsFMSAttached():
+                raise
+        
+        
+        # low level stuff last
+        
+        try:
+            camera.Update()
+        except:
+            if not self.ds.IsFMSAttached():
+                raise
+        
+        try:
+            susan.Update()
+        except:
+            if not self.ds.IsFMSAttached():
+                raise
+        
+        try:
+            wheel.Update()
+        except:
+            if not self.ds.IsFMSAttached():
+                raise
+        
+        try:
+            chamber.Update()
+        except:
+            if not self.ds.IsFMSAttached():
+                raise
+                
+        try:
+            feeder.Update()
+        except:
+            if not self.ds.IsFMSAttached():
+                raise
+
+        try:
+            rampArm.Update()    
+        except:
+            if not self.ds.IsFMSAttached():
+                raise
+        
+        #
+        # These two only get called when there is new data received
+        # from the driver station
+        #
+        
+        if self.ds.IsNewControlData():
+        
+            try:
+                consoleLed.Update()
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise
+    
+            try:
+                operatorLeds.Update()
+            except:
+                if not self.ds.IsFMSAttached():
+                    raise
         
 def run(): 
     #rpdb2.start_embedded_debugger()

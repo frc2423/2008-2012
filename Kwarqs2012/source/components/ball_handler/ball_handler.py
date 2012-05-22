f2ff0abe61e54d0942dc28fd868a9f298ae37b38
Dustin Spicuzza <dustin@virtualroadside.com>
2012-05-22 00:30:35 +0000

    Adjusted the ball handler to move the balls correctly depending on the sensor state
    git-svn-id: https://kwarqs-dev.bbn.com/svn/kwarqs/code@841 eb211378-2a44-443b-ae4a-d45323d45073

--- Kwarqs2012/source/components/ball_handler/ball_handler.py
+++ Kwarqs2012/source/components/ball_handler/ball_handler.py
@@ -43,12 +43,14 @@ class BallHandler(object):
         self.chamber.Run()
         
     def Print(self):
-        print("BallHandler: FLO: %s, FV: %s, FTOP: %s, CFUL: %s" % 
-            (self.last_feeder_low, self.feeder_virtual_ball, self.last_feeder_top,
-             self.last_chamber_full))
-        print("Voltage: Feeder: %.3f Chamber: %.3f" % (
-            self.feeder.top_ball_sensor.irSensor.GetVoltage(),
-            self.chamber.ball_sensor.irSensor.GetVoltage() ))
+        pass
+        
+        #print("BallHandler: FLO: %s, FV: %s, FTOP: %s, CFUL: %s" % 
+        #    (self.last_feeder_low, self.feeder_virtual_ball, self.last_feeder_top,
+        #     self.last_chamber_full))
+        #print("Voltage: Feeder: %.3f Chamber: %.3f" % (
+        #    self.feeder.top_ball_sensor.irSensor.GetVoltage(),
+        #    self.chamber.ball_sensor.irSensor.GetVoltage() ))
         
         
     def Update(self):
@@ -61,36 +63,63 @@ class BallHandler(object):
         #
         
         # query ball sensors
-        chamber_full = self.chamber.IsFull()
-        feeder_top = self.feeder.HasTopBall()
-        feeder_middle  = self.feeder.HasMidBall()
-        feeder_low = self.feeder.HasLowBall()
-        feeder_enter = self.feeder.HasEnterBall()
+        chamber_full =  bool(self.chamber.IsFull())
+        feeder_top =    bool(self.feeder.HasTopBall())
+        feeder_middle = bool(self.feeder.HasMidBall())
+        feeder_low =    bool(self.feeder.HasLowBall())
+        feeder_enter =  bool(self.feeder.HasEnterBall())
         
         # setup widget
         self.widget.SetAll( chamber_full, feeder_top, feeder_middle, feeder_low, feeder_enter )
         
         feeder_full = feeder_top and feeder_middle and feeder_low
-        has_ball = feeder_enter or feeder_top or feeder_low or feeder_middle
         
         # automated feeding
     
-        if auto_feed:
+        if auto_feed or continuous_feed:
             
             feed = False
             run_chamber = False
             
+            # only feed when there are balls that can continue onwards
+            # -> you can solve this using a truth table and a Karnaugh map
+            # 
+            #   E   L   M   T   |   F
+            #  -----------------|-----
+            #   -   -   -   -   |   -
+            #   -   -   -   X   |   -
+            #   -   -   X   -   |   X
+            #   -   -   X   X   |   -
+            #   -   X   -   -   |   X
+            #   -   X   -   X   |   X
+            #   -   X   X   -   |   X
+            #   -   X   X   X   |   -
+            #   X   -   -   -   |   X
+            #   X   -   -   X   |   X
+            #   X   -   X   -   |   X
+            #   X   -   X   X   |   X
+            #   X   X   -   -   |   X
+            #   X   X   -   X   |   X
+            #   X   X   X   -   |   X
+            #   X   X   X   X   |   -
+            #
             
-            if (continuous_feed or has_ball or feeder_enter) and not feeder_full:
+            feed = (feeder_enter and not feeder_low) or \
+                   (feeder_middle and not feeder_top) or \
+                   (feeder_low and not feeder_middle)
+            
+            # enable the continuous feed functionality if required
+            if continuous_feed and not feeder_full:
                 feed = True
             
-            
+            # transfer the ball from the feeder to the chamber
             if feeder_top and not chamber_full:
                 feed = True
                 run_chamber = True
             
-            if chamber_full:
-                run_chamber = False
+            # this is redundant
+            #if chamber_full:
+            #    run_chamber = False
             
             #
             # Do it (unless the user already did something)

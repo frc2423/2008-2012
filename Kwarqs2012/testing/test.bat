329fa201be7e00f1df76da70ee7774129a65c117
Dustin Spicuzza <dustin@virtualroadside.com>
2012-10-19 20:48:58 -0400

    Detect python3 correctly

--- Kwarqs2012/testing/test.bat
+++ Kwarqs2012/testing/test.bat
@@ -2,7 +2,19 @@
 
 set ec=1
 
-c:\Python32\python.exe -B "%~dp0\test.py" %*
+set PYTHON_EXE=python.exe
+
+set PYTHON_BIN=C:\Python33\%PYTHON_EXE%
+if exist %PYTHON_BIN% goto python_found
+
+set PYTHON_BIN=C:\Python32\%PYTHON_EXE%
+if exist %PYTHON_BIN% goto python_found
+
+echo Error: Could not find python 3
+exit /b 1
+
+:python_found
+%PYTHON_BIN% -B "%~dp0\test.py" %*
 
 if "%errorlevel%" == "0" set ec=0
 pause
